913911030f6c2d28a013c4e0489111853fbafd8d
Sumin Ha <minha62@ewhain.net>
2022-11-07 14:15:13 +0900

    Add code to check the progress

--- convert_pcd2bin.py
+++ convert_pcd2bin.py
@@ -1,5 +1,5 @@
 import pclpy
-import pclpy.pcl as pcl
+from pclpy import pcl
 import numpy as np
 import os
 import glob
@@ -15,7 +15,7 @@ def load_pc_from_pcd(input_path):
     pcdreader = pcl.io.PCDReader()
     pc = pcl.PointCloud.PointXYZ()
 
-    pcd = pcdreader.read(input_path, pc)
+    pcdreader.read(input_path, pc)
     points = pc.xyz
 
     p = from_xyz_to_xyzi(points)
@@ -26,16 +26,23 @@ def save_bin_from_pc(points, bin_path):
 
 
 #file_list = glob.glob(f'/home/smha/vs-nas/OpenPCDet/data/kitti1101/training/pcd/*.pcd', recursive=True)
-file_list = os.listdir('home/smha/vs-nas/OpenPCDet/data/kitti1101/training/pcd/*.pcd')
+# file_path = '/home/smha/vs-nas/OpenPCDet/data/kitti1101/training/pcd/'
+file_path = '/home/smha/vs-nas/OpenPCDet/sunset_cloudy_normal_20220803_094139_santafe/pcd/'
+file_list = os.listdir(file_path)
+file_num = len(file_list)
+i = 1
 
-for file_path in file_list:
-    points = load_pc_from_pcd(file_path)
+for file in file_list:
+    print(f"{i} / {file_num}")
+    i += 1
+    points = load_pc_from_pcd(file_path + file)
+    # print('file_path',file_path)
 
     split_list = file_path.split("/")
-    bin_path = split_list[0]+"/"+split_list[1]+"/"+split_list[2]+"/"+split_list[3]+"/"+split_list[4]+"/"+"new_data"+"/"+"kitti_1103"+"/"+"bin"+"/"
-
+    # bin_path = split_list[0]+"/"+split_list[1]+"/"+split_list[2]+"/"+split_list[3]+"/"+split_list[4]+"/"+"new_data"+"/"+"kitti_1103"+"/"+"bin"+"/"
+    bin_path = split_list[0]+"/"+split_list[1]+"/"+split_list[2]+"/"+split_list[3]+"/"+split_list[4]+"/"+"new_data"+"/"+"kitti_1107"+"/"+split_list[5]+"/"+"bin"+"/"
     if not os.path.exists(bin_path):
         os.makedirs(bin_path)
 
-    bin_name = split_list[8].replace('.pcd', '.bin')
+    bin_name = file.replace('.pcd', '.bin')
     save_bin_from_pc(points, bin_path + bin_name)
