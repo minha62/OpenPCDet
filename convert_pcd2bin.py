ecce7c69836d4639895f59b24b864fe3266cea7b
Sumin Ha <minha62@ewhain.net>
2022-12-08 12:39:36 +0900

    Update convert_pcd2bin.py

--- convert_pcd2bin.py
+++ convert_pcd2bin.py
@@ -1,9 +1,17 @@
+# command: $ sudo ~/anaconda3/envs/pclpy/bin/python convert_pcd2bin.py --path '${parent folder path of pcd folder}'
+
 import pclpy
-from pclpy import pcl
+import pclpy.pcl as pcl
 import numpy as np
 import os
 import glob
 import shutil
+from absl import app, flags
+from absl.flags import FLAGS
+
+
+flags.DEFINE_string('path', '/home/smha/ts_backup2_249/OpenPCDet/gumsu_ori_data/kitti/training/20221020/', 'parent folder path of pcd folder')
+
 
 def from_xyz_to_xyzi(points):
     row, column = points.shape
@@ -11,38 +19,49 @@ def from_xyz_to_xyzi(points):
     points = np.concatenate((points, intensity), axis = 1)
     return points
 
+
 def load_pc_from_pcd(input_path):
     pcdreader = pcl.io.PCDReader()
     pc = pcl.PointCloud.PointXYZ()
 
-    pcdreader.read(input_path, pc)
+    pcd = pcdreader.read(input_path, pc)
     points = pc.xyz
 
     p = from_xyz_to_xyzi(points)
     return np.array(list(p), dtype = np.float32)
 
+
 def save_bin_from_pc(points, bin_path):
     points.tofile(bin_path)
 
 
-#file_list = glob.glob(f'/home/smha/vs-nas/OpenPCDet/data/kitti1101/training/pcd/*.pcd', recursive=True)
-# file_path = '/home/smha/vs-nas/OpenPCDet/data/kitti1101/training/pcd/'
-file_path = '/home/smha/vs-nas/OpenPCDet/sunset_cloudy_normal_20220803_094139_santafe/pcd/'
-file_list = os.listdir(file_path)
-file_num = len(file_list)
-i = 1
+def main(argv):
+    pcd_path = os.path.join(FLAGS.path, 'pcd/*.pcd')
 
-for file in file_list:
-    print(f"{i} / {file_num}")
-    i += 1
-    points = load_pc_from_pcd(file_path + file)
-    # print('file_path',file_path)
+    # list of pcd files' path
+    pcd_list = glob.glob(pcd_path, recursive=True)
 
-    split_list = file_path.split("/")
-    # bin_path = split_list[0]+"/"+split_list[1]+"/"+split_list[2]+"/"+split_list[3]+"/"+split_list[4]+"/"+"new_data"+"/"+"kitti_1103"+"/"+"bin"+"/"
-    bin_path = split_list[0]+"/"+split_list[1]+"/"+split_list[2]+"/"+split_list[3]+"/"+split_list[4]+"/"+"new_data"+"/"+"kitti_1107"+"/"+split_list[5]+"/"+"bin"+"/"
-    if not os.path.exists(bin_path):
-        os.makedirs(bin_path)
+    i = 1 # for checking the progress of conversion
 
-    bin_name = file.replace('.pcd', '.bin')
-    save_bin_from_pc(points, bin_path + bin_name)
+    for pcd in pcd_list:
+        # load xyzi points from pcd
+        points = load_pc_from_pcd(pcd)
+
+        bin_path = os.path.join(FLAGS.path, 'velodyne/')
+
+        if not os.path.exists(bin_path):
+            os.makedirs(bin_path)
+
+        
+        splited_pcd_path = pcd.split("/")
+
+        # convert the file extension (PCD -> BIN)
+        bin_name = splited_pcd_path[-1].replace('.pcd', '.bin')
+        
+        save_bin_from_pc(points, bin_path + bin_name)
+
+        # print progress
+        print(f'{i} / {len(file_list)}')
+        i += 1
+
+app.run(main)
